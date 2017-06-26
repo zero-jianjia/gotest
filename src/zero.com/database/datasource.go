11af242f3c143326e153474f2cb06f912d30b898
jianjia1 <jianjia1@staff.sina.com.cn>
2017-06-26 11:52:23 +0800

    test

--- src/zero.com/database/datasource.go
+++ src/zero.com/database/datasource.go
@@ -6,48 +6,106 @@ import (
     "fmt"
 )
 
+var db, _ = sql.Open("mysql", "dsp_user:123456@tcp(10.210.228.92:3307)/jianjia?charset=utf8")
+
 func main() {
-    db, err := sql.Open("mysql", "dsp_user:123456@tcp(10.210.228.92:3307)/jianjia?charset=utf8")
-    checkErr(err)
-
-    rows, err := db.Query("SELECT id FROM adunit limit 5")
-
-    //for rows.Next() {
-    //    var id string
+    //test1()
+    test2()
+    //rows, err := db.Query("SELECT * FROM adunit limit 2")
     //
-    //    rows.Columns()
-    //    err = rows.Scan(&id)
+    //for rows.Next() {
+    //
+    //    column, _ := rows.Columns()//所有的列名
+    //    //fmt.Println(column)
+    //    values := make([][]byte, len(column))
+    //    scans := make([]interface{}, len(column))
+    //    for i := range values {
+    //        //让每一行数据都填充到[][]byte里面
+    //        scans[i] = &values[i]
+    //    }
+    //    err = rows.Scan(scans ...)
     //    checkErr(err)
-    //    fmt.Println(id)
+    //
+    //    row := make(map[string]string) //每行数据
+    //    for rows.Next() {
+    //        //循环，让游标往下移动
+    //        for k, v := range values {
+    //            //每行数据是放在values里面，现在把它挪到row里
+    //            key := column[k]
+    //            row[key] = string(v)
+    //        }
+    //    }
+    //
+    //    fmt.Println(row)
     //}
 
-    column, _ := rows.Columns()  //读出查询出的列字段名
-    values := make([][]byte, len(column))   //values是每个列的值，这里获取到byte里
-    scans := make([]interface{}, len(column)) //因为每次查询出来的列是不定长的，用len(column)定住当次查询的长度
-    for i := range values {    //让每一行数据都填充到[][]byte里面
-        scans[i] = &values[i]
-    }
-    results := make(map[int]map[string]string) //最后得到的map
-    i := 0
-    for rows.Next() { //循环，让游标往下移动
-        if err := rows.Scan(scans...); err != nil { //query.Scan查询出来的不定长值放到scans[i] = &values[i],也就是每行都放在values里
-            fmt.Println(err)
-            return
-        }
-        row := make(map[string]string) //每行数据
-        for k, v := range values {     //每行数据是放在values里面，现在把它挪到row里
-            key := column[k]
-            row[key] = string(v)
-        }
-        results[i] = row //装入结果集中
-        i++
-    }
-    for k, v := range results { //查询出来的数组
-        fmt.Println(k, v)
+    //column, _ := rows.Columns()  //读出查询出的列字段名
+    //values := make([][]byte, len(column))   //values是每个列的值，这里获取到byte里
+    //scans := make([]interface{}, len(column)) //因为每次查询出来的列是不定长的，用len(column)定住当次查询的长度
+    //for i := range values {    //让每一行数据都填充到[][]byte里面
+    //    scans[i] = &values[i]
+    //}
+    //results := make(map[int]map[string]string) //最后得到的map
+    //i := 0
+    //for rows.Next() { //循环，让游标往下移动
+    //    if err := rows.Scan(scans...); err != nil { //query.Scan查询出来的不定长值放到scans[i] = &values[i],也就是每行都放在values里
+    //        fmt.Println(err)
+    //        return
+    //    }
+    //    row := make(map[string]string) //每行数据
+    //    for k, v := range values {     //每行数据是放在values里面，现在把它挪到row里
+    //        key := column[k]
+    //        row[key] = string(v)
+    //    }
+    //    results[i] = row //装入结果集中
+    //    i++
+    //}
+    //for k, v := range results { //查询出来的数组
+    //    fmt.Println(k, v)
+    //}
+    //
+    //db.Close()
+
+}
+
+func test2() {
+    rows, _ := db.Query("SELECT id,updateTime FROM adunit limit 2")
+    columns, _ := rows.Columns()  //读出查询出的列字段名
+    //fmt.Println(columns)
+
+    scanArgs := make([]interface{}, len(columns))
+    values := make([]interface{}, len(columns))
+    for i := range values {
+        scanArgs[i] = &values[i]
     }
 
-    db.Close()
+    for rows.Next() {
+        //将行数据保存到record字典
+        rows.Scan(scanArgs...)
+        record := make(map[string]string)
+        for i, col := range values {
+            if col != nil {
+                record[columns[i]] = string(col.([]byte))
+            }
+        }
+        fmt.Println(record)
+    }
 
+    defer rows.Close()
+}
+
+func test1() {
+    rows, err := db.Query("SELECT id,updateTime FROM adunit limit 2")
+    for rows.Next() {
+        var id string
+        var updateTime string
+        rows.Columns()
+        err = rows.Scan(&id, &updateTime)
+        checkErr(err)
+        fmt.Println(id)
+        fmt.Println(updateTime)
+    }
+    defer rows.Close()
 }
 
 func checkErr(errMasg error) {
